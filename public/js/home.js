08b0d670d0edbd47cf9209536920eb4773cd2294
kirtanchandak <themarketingkirtan@gmail.com>
2023-03-30 18:06:28 +0530

    post req via ajax req

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,14 @@
 $(".submit-btn").click(function () {
-  alert("ok");
+  let data = {
+    username: $("#username").val(),
+    roomid: $("#roomid").val(),
+  };
+  if (data.name === "" || data.roomid === "") {
+    alert("Please enter a username and roomid");
+    return;
+  } else {
+    $.post("/join-room", data, function (res) {
+      console.log(res);
+    });
+  }
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("pages/index");
+});
+
+app.post("/join-room", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(5000, () => {
